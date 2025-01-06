a9f1270695759bcb1ce4666ddff171db194f2e10
TranManhLinh <tranmanhlinh204@gmail.com>
2025-01-06 16:55:59 +0700

    update

--- index.js
+++ index.js
@@ -25,3 +25,14 @@ document.getElementById('search-icon').addEventListener('click', function () {
 });
 
 
+document.addEventListener("DOMContentLoaded", function () {
+  const images = document.querySelectorAll(".slider-image");
+  let currentIndex = 0;
+
+  function showNextImage() {
+    images[currentIndex].classList.remove("active");
+    currentIndex = (currentIndex + 1) % images.length;
+    images[currentIndex].classList.add("active");
+  }
+  setInterval(showNextImage, 3000);
+});
